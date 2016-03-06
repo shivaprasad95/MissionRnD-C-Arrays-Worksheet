302e7e1ae446c068604ab8df84ef6308d617925b
shivaprasad95 <shivaprasad95@missionrnd.com>
2016-03-06 14:20:04 +0530

    Submit Commit

--- src/scoresDescendingSort.cpp
+++ src/scoresDescendingSort.cpp
@@ -21,5 +21,32 @@ struct student {
 };
 
 void * scoresDescendingSort(struct student *students, int len) {
-	return NULL;
+	int index1 = 0, index2 = len - 1;
+	int i, j, flag1 = 0, flag2 = 0;
+	struct student temp;
+	if (len < 0 || students == 0)
+		return 0;
+
+	for (i = 0, j = len - 1; i<len&&j>i; i++, j--)
+	{
+		if (students[i + 1].score>students[i].score){
+			index1 = i;
+			flag1 = 1;
+		}
+		if (students[j - 1].score<students[j].score)
+		{
+			index2 = j;
+			flag2 = 1;
+		}
+		if (flag1&&flag2)
+			break;
+	}
+
+	if (index1 == 0 && index2 == len - 1 && flag1 == 0 && flag2 == 0)
+		return 0;
+
+
+	temp = students[index1];
+	students[index1] = students[index2];
+	students[index2] = temp;
 }
